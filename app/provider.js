bc8f7b441cb6ef017115c14b08ab5da808abe0bb
jayesh-2404 <jayeshpatil9028@gmail.com>
2025-03-09 23:46:21 +0530

    update

--- app/provider.js
+++ app/provider.js
@@ -4,12 +4,16 @@ import {ConvexReactClient} from 'convex/react'
 import {ConvexProvider} from 'convex/react'
 
 function Provider({children}) {
-    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+    const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+    if (!convexUrl) {
+        throw new Error(
+            "NEXT_PUBLIC_CONVEX_URL is not set - make sure you've run `npx convex dev` or set the URL manually"
+        );
+    }
+    const convex = new ConvexReactClient(convexUrl);
     return (
-    <div>
-       <ConvexProvider client={convex}>{children}</ConvexProvider>
-    </div>
-  )
+        <ConvexProvider client={convex}>{children}</ConvexProvider>
+    )
 }
 
 export default Provider
